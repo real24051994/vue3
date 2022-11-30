e180bec3ac73962128a8daccf8d4502808c3222a
khanhtn <khanhtn@firegroup.io>
2022-11-30 21:53:04 +0700

    update

---
+++ src/modules/views/layout.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="page-feed-center">
+		<transition name="fade-in" mode="out-in">
+			<router-view />
+		</transition>
+	</div>
+</template>
+<script>
+	export default {
+		name: "VueLayout",
+		data() {
+			return {};
+		},
+	};
+</script>
